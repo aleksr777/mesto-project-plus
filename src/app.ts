a5aaa7b7062975521514153829d40a67f7a5ab4d
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-10-01 20:27:29 +0500

    feat: Создал временный мидлвар для авторизации

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import router from './routes/routes'
 import mongoose from 'mongoose'
 
 const { PORT = 3000 } = process.env
 const app = express()
-app.use(express.json())
+app.use( express.json() )
 
 // Подключение к MongoDB
 mongoose.connect( 'mongodb://localhost:27017/mestodb' )
@@ -21,6 +21,26 @@ db.once( 'open', () => {
 
 db.on( 'disconnected', () => {
   console.log( 'Disconnected from MongoDB' )
+} )
+
+// Объявляем пользовательский тип для объекта user
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        _id: string
+      }
+    }
+  }
+}
+
+// Временный мидлвар для авторизации
+app.use( ( req: Request, res: Response, next: NextFunction ) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // Здесь нужно будет использовать настоящий идентификатор пользователя
+  }
+
+  next()
 } )
 
 app.use( '/', router )
