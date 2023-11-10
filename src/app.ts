f2085658da78d62069bdeb420919a0624ae9749c
Aleksandr Petrov <petrov.aleksr@gmail.com>
2023-11-10 22:28:42 +0500

    fix: поправка типизации временного мидлвара для авторизации

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express, {
+  Request,
   Response,
   NextFunction,
 } from 'express';
 import connectToDatabase from './utils/connect-to-database';
 import routes from './routes/index';
+
+/* Для временного обхода ошибок типизации */
+declare global {
+  namespace Express {
+    interface Request {
+      user: {
+        _id: string;
+      };
+    }
+  }
+}
 
 const { PORT = 3000 } = process.env;
 const app: express.Express = express();
@@ -13,7 +25,7 @@ app.use(express.json());
 connectToDatabase();
 
 // Временный мидлвар для авторизации
-app.use((req: any, res: Response, next: NextFunction) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   req.user = {
     _id: '65198bdc11e4cfb283c97609',
   };
